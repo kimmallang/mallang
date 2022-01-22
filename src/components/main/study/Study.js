e1983e52d719c61d7e5b1ca72e7feab5e7951a79
kimmallang <kimmallangcp@gmail.com>
2022-01-22 19:52:40 +0900

    update : [스터디] StudyApi.js 구현 > 임시 데이터 사용

--- src/components/main/study/Study.js
+++ src/components/main/study/Study.js
@@ -1,56 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "../../common/card/Card";
+import StudyApi from "./StudyApi";
 
 function Study() {
-    const [studyList] = useState([
-        {
-            id: 1,
-            datetime: '2022-01-17 08:59:01',
-            title: '스터디 구합니다.',
-            description: 'Hello My Name Is Dongju.',
-            commentsCount: 456,
-        },
-        {
-            id: 2,
-            datetime: '2022-01-17 08:59:01',
-            title: '스터디 구합니다.',
-            description: 'Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju.',
-            commentsCount: 0,
-        },
-        {
-            id: 3,
-            datetime: '2022-01-17 08:59:01',
-            title: '스터디 구합니다.',
-            description: 'Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju.Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju.Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju. Hello My Name Is Dongju.',
-            commentsCount: 1,
-        },
-        {
-            id: 4,
-            datetime: '2022-01-17 08:59:01',
-            title: '스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. 스터디 구합니다. ',
-            description: 'Hello My Name Is Dongju.',
-            commentsCount: 999,
-        },
-        {
-            id: 5,
-            datetime: '2022-01-17 08:59:01',
-            title: '스터디 구합니다.',
-            description: '안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요.안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요.안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요.안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요.안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요. 안녕하세요.',
-            commentsCount: 1000,
-        },
-        {
-            id: 6,
-            datetime: '2022-01-17 08:59:01',
-            title: '스터디 구합니다.',
-            description: 'Hello My Name Is Dongju.',
-            commentsCount: 1234,
-        },
-    ]);
+    const [studies, setStudies] = useState([]);
+
+    useEffect(() => {
+        StudyApi.getStudies()
+            .then((studies) => setStudies(studies))
+            .catch(e => console.log(e));
+    });
 
     return (
         <div className="study">
             {
-                studyList.map(({ datetime, title, description, commentsCount }, index) => (
+                studies.map(({ datetime, title, description, commentsCount }, index) => (
                     <Card datetime={datetime}
                           title={title}
                           description={description}
