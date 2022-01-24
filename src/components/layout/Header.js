d5aaa4df48d7918073c41df12ee036d34860a596
kimmallang <kimmallangcp@gmail.com>
2022-01-24 23:51:56 +0900

    add : [헤더] 메뉴 펼치기 아이콘 추가

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import TopLogo from "./header/components/TopLogo";
 import Menus from "./header/Menus";
+import MenuIcon from "./header/MenuIcon";
 
 function Header() {
     return (
         <header>
             <div>
+                <MenuIcon />
                 <TopLogo />
                 <Menus />
             </div>
---
+++ src/components/layout/header/MenuIcon.js
@@ -0,0 +1,11 @@
+import { CgMenuGridR } from "react-icons/cg";
+
+function MenuIcon() {
+    return (
+        <div className='menu-icon'>
+            <CgMenuGridR />
+        </div>
+    );
+}
+
+export default MenuIcon;
