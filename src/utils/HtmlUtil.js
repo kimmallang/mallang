79e9d9644970c5d99eaef14dcf9790092c03d7c7
kimmallang <kimmallangcp@gmail.com>
2022-02-18 23:58:07 +0900

    update : [뉴스] 제목, 내용 html 텍스트 처리

--- src/utils/HtmlUtil.js
+++ src/utils/HtmlUtil.js
@@ -33,6 +33,24 @@ const HtmlUtil = {
     'hasBodyClassName': (className) => {
         return window.document.body.className.includes(className);
     },
+
+    'textToHtml': (htmlText) => {
+        return htmlText.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, '') // html 태그 제거
+            .replace(/&nbsp;/gi,' ')
+            .replace(/&quot;/gi,'"')
+            .replace(/&amp;/gi,'&')
+            .replace(/&lt;/gi,'<')
+            .replace(/&gt;/gi,'>')
+            .replace(/&circ;/gi,'ˆ')
+            .replace(/&lsquo;/gi,'‘')
+            .replace(/&rsquo;/gi,'’')
+            .replace(/&sbquo;/gi,'‚')
+            .replace(/&ldquo;/gi,'“')
+            .replace(/&rdquo;/gi,'”')
+            .replace(/&bdquo;/gi,'„')
+            .replace(/&permil;/gi,'‰')
+            .replace(/&euro;/gi,'€');
+    },
 };
 
 export default HtmlUtil;
